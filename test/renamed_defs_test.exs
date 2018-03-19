cdd89d496743631edda78aae7a1de06ebace8111
Michael Oliver <moliver@pinterest.com>
2018-03-19 13:34:48 -0700

    Fix bug with float to charlist in Elixir 1.1 and 1.2

--- test/renamed_defs_test.exs
+++ test/renamed_defs_test.exs
@@ -18,12 +18,12 @@ defmodule RenamedDefsTest do
 
   test "Float.to_charlist and Float.to_char_list" do
     assert capture_io(:stderr, fn ->
-      {'-2.7', _} = Code.eval_string("""
+      {'-2.7' ++ _, _} = Code.eval_string("""
       import ExCompatible
       safe(Float.to_charlist(-2.7))
       """)
 
-      {'1.99', _} = Code.eval_string("""
+      {'1.99' ++ _, _} = Code.eval_string("""
       import ExCompatible
       safe(Float.to_char_list(1.99))
       """)
